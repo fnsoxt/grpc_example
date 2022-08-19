b7812475515afe004234ccc7d34a90ca282165be
zhangzhijie <zhangzhijie@haohaozhu.com>
2022-08-19 18:49:16 +0800

    gateway

--- server/server.go
+++ server/server.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 	service "hello_grpc/service"
 	"net"
+	"net/http"
 )
 
 type server struct{}
@@ -31,9 +34,38 @@ func main() {
 
 	reflection.Register(s)
 
-	err = s.Serve(lis)
+	go func() {
+		err = s.Serve(lis)
+		if err != nil {
+			fmt.Printf("开启服务失败: %s", err)
+			return
+		}
+	}()
+
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
-		fmt.Printf("开启服务失败: %s", err)
-		return
+		fmt.Println("Failed to dial server:", err)
 	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = service.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		fmt.Println("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	fmt.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	fmt.Println(gwServer.ListenAndServe())
 }
